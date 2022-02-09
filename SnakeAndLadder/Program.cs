493a2b871222ebf38829d2c8f7f1a83289d9b370
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-09 11:12:35 +0530

    [akhil]uc1

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -2,11 +2,15 @@
 
 namespace SnakeAndLadder
 {
+    /* UC1:- Snake and Ladder game played with single player at start position 0
+     */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***Welcome To Snake and Ladder Gmae***");
+            int playerPosition = 0;
+            Console.WriteLine("Player Position: " + playerPosition);
         }
     }
 }
